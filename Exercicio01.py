bbe4e1b81dbacff06870b5d081fcd991755310c2
Matheus Pereira Vidal <matheus.jessy1217@gmail.com>
2026-03-25 20:48:29 -0300

    terceiro commit

--- Exercicio01.py
+++ Exercicio01.py
@@ -2,4 +2,6 @@ valor = float(input('Digite um valor: '))
 if valor > 0:
     print(f'O numero é positivo!')
 else:
-    print(f'O numero não é positivo!')
+    print(f'O numero não é positivo!') 
+
+    print('salve')
